26b084f28ec6975f7b264dd3ff0c35309e3b3f97
OlegFS <o.vinogradov.92@gmail.com>
2016-04-14 19:26:57 +0200

    The brand new connectivity maker
    I added  different shapes and changed the structure a bit. + one internal function.

--- connectivity.m
+++ connectivity.m
@@ -1,4 +1,4 @@
-function [M,W] = connectivity(varargin)
+function [W] = connectivity(varargin)
 % Conncectivity function
 % Creates the initial electiode grid of the size n and corresponding number
 % of connectivity matrices. 
@@ -7,9 +7,9 @@ function [M,W] = connectivity(varargin)
 %           2) Weightsize - size of the connectivity matrix. Has to be odd. 
 %              default - 13
 %           3) Number of descendants. default - 1.
-%               Given as a power of the vector norm;
-%               1-for one; 1.3 - for two; 1.6 - for three; 2.2 - for five
-%           4) Scale of the Gaussian(sigma). default - 1.
+%           4) Shape of the connections. 1- Gaussian, 2- Bowl Shape, 
+%               3- random field. 
+%           4) Spread of the Gaussian(sigma). default - 1.
 %           5) Plot model of weights - 1or 0; default - 0.
 %           6) Plot full set of weights - 1 or 0; default - 0. (Not
 %           recommended)
@@ -22,17 +22,18 @@ function [M,W] = connectivity(varargin)
 % Example: 
 % [M,W] = connectivity(10,10,1,1);
 
-
-if length(varargin) < 6
+if length(varargin)<7;
     plot1 = 0;
-    if  length(varargin) <5;
-        plot2 = 0;
-        if  length(varargin) < 4;
-            scale = 1;
-             if  length(varargin)<3;
+    if length(varargin) < 6
+    plot2 = 0;
+        if  length(varargin) <5;
+        scale = 1;
+            if  length(varargin) < 4;
+                Connectivity = 1;
+                if  length(varargin)<3;
                  descendants = 1;
                      if  length(varargin)<2
-                        weightsize = 13;
+                        weightsize = 27;
                          if isempty(varargin)
                      gridsize = 13;
                          end
@@ -40,16 +41,19 @@ if length(varargin) < 6
              end
         end
     end
+end
 else 
-   
+    
     gridsize = varargin{1};
     weightsize =varargin{2};
     descendants = varargin{3};
-    scale = varargin{4}; 
-    plot1 = varargin{5};
-    plot2 = varargin{6};
+    connectivity = varargin{4};
+    scale = varargin{5}; 
+    plot1 = varargin{6};
+    plot2 = varargin{7};
     
 end
+
 
 if mod(weightsize,2) ==0
     error('Weight Matrix has to be odd')
@@ -57,20 +61,65 @@ end
 
 
 M = zeros(gridsize); % Electrode Grid HAS TO BE ODD
-x= linspace(-3,3,weightsize);
 
-  
+% Preallocation of the weights matrix
+W = zeros(gridsize,gridsize,[gridsize*gridsize]);
+
+
+switch connectivity
+    % Normal Gaussian
+    case 1
+        
+x= linspace(-3,3,weightsize);
 % Gaussian Matrix of Weights; A model for convolution
 G = exp(-((meshgrid(x).^2+meshgrid(x).^2')/scale));
 % Setting the connection to itself to 0
 G(ceil(size(G,1)/2),ceil(size(G,2)/2))=0; 
-% Normalizig the model
-n= reshape(G,length(G)^2,1);
-n = n/norm(n,descendants);
-G = reshape(n,length(G),length(G));
-% Preallocation of the weights matrix
-W = zeros(gridsize,gridsize,[gridsize*gridsize]);
 
+W= convW(M,W,G,descendants,gridsize);
+
+    % Bowl-shape
+    case 2
+        
+x= linspace(-3,3,weightsize);
+% Gaussian Matrix of Weights; A model for convolution
+G = 1-exp(-((meshgrid(x).^2+meshgrid(x).^2')/scale));
+% Setting the connection to itself to 0
+G(ceil(size(G,1)/2),ceil(size(G,2)/2))=0; 
+W= convW(M,W,G,descendants,gridsize);
+
+    % Random
+    case 3
+    for i = 1:size(W,3)
+    W(:,:,i) = abs(randn(gridsize));
+    W(:,:,i)=(W(:,:,i)/ sum(sum(W(:,:,i)))).*descendants;
+    end
+        
+end
+
+
+switch plot1
+    case 1
+        figure;
+        imagesc(W(:,:,ceil(length(W)/2)));
+    case 0 
+end
+
+switch plot2
+    case 1
+        figure;
+       for i=1:gridsize^2;
+    subplot(gridsize,gridsize,i);
+    imagesc(W(:,:,i));
+       end
+    case 0 
+end
+        
+
+
+
+
+function W = convW(M,W,G,descendants,gridsize);
 % Convolution
 % Creating the weight matrix for each electrode in the gridl
 i = 1;
@@ -81,28 +130,9 @@ for c =1:length(M);
         i=find(M);
         W(:,:,i) =conv2(M,G,'same');
         % Check for the number of descendants for the border electrodes
-        if sum(sum(W(:,:,i))) < 1;
-            n= reshape(W(:,:,i),length(W(:,:,i))^2,1);
-            n = n/norm(n,descendants);
-            W(:,:,i) = reshape(n,length(W(:,:,i)),length(W(:,:,i)));
+        % Normalize the W to sum up to descendants number
+        if sum(sum(W(:,:,i))) ~ descendants;
+            W(:,:,i) = (W(:,:,i)./sum(sum(W(:,:,i)))).*descendants;
          end
     end
 end
-
-switch plot1
-    case 1
-        figure;
-        imagesc(G);
-    case 0 
-end
-
-switch plot2
-    case 1
-        figure;
-       for i=1:gridsize^2;
-    subplot(gridsize,gridsize,i);
-    imagesc(W(:,:,i));
-end
-    case 0 
-end
-        
